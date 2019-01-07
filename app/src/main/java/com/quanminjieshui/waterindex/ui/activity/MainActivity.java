88a4bf98c4fd0236ff7dbcec26b583fdebe122e8
fushizhe <fushizhe@sina.cn>
2019-01-08 00:11:13 +0800

    update

--- app/src/main/java/com/quanminjieshui/waterindex/ui/activity/MainActivity.java
+++ app/src/main/java/com/quanminjieshui/waterindex/ui/activity/MainActivity.java
@@ -1,8 +1,6 @@
 package com.quanminjieshui.waterindex.ui.activity;
 
 import android.content.Intent;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -17,7 +15,6 @@ import android.widget.TextView;
 import com.quanminjieshui.waterindex.R;
 import com.quanminjieshui.waterindex.base.ActivityManager;
 import com.quanminjieshui.waterindex.base.BaseActivity;
-import com.quanminjieshui.waterindex.event.LoginStatusChangedEvent;
 import com.quanminjieshui.waterindex.event.SelectFragmentEvent;
 import com.quanminjieshui.waterindex.ui.fragment.FindFragment;
 import com.quanminjieshui.waterindex.ui.fragment.PersonalFragment;
@@ -25,8 +22,6 @@ import com.quanminjieshui.waterindex.ui.fragment.TransactionFragment;
 import com.quanminjieshui.waterindex.utils.SPUtil;
 import com.quanminjieshui.waterindex.utils.StatusBarUtil;
 import com.quanminjieshui.waterindex.utils.ToastUtils;
-import com.zhy.autolayout.utils.AutoUtils;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -57,9 +52,6 @@ public class MainActivity extends BaseActivity {
 
     private Fragment currentFragment,tab1,tab2,tab3;
     private long mExitTime;
-
-//    RadioButton[] rb = new RadioButton[5];
-//    Drawable drawables[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,28 +98,6 @@ public class MainActivity extends BaseActivity {
 
     private void initView() {
         createFragments();
-        initRbsize();
-    }
-
-
-    private void initRbsize() {
-//        rb[0] = rb3;
-//        rb[1] = rb4;
-//        rb[2] = rb5;
-//
-//        for (int i = 0; i < rb.length; i++) {
-//            //挨着给每个RadioButton加入drawable限制边距以控制显示大小
-//            drawables = rb[i].getCompoundDrawables();
-//            //获取drawables，2/5表示图片要缩小的比例
-//            Rect r = new Rect(0, 0, drawables[1].getMinimumWidth() * 2 / 3, drawables[1].getMinimumHeight() * 2 / 3);
-//            //定义一个Rect边界
-//            drawables[1].setBounds(r);
-//            //给每一个RadioButton设置图片大小
-//            rb[i].setCompoundDrawables(null, drawables[1], null, null);
-////            rb[i].setTextSize(Util.px2sp(this,24,1334));
-//            AutoUtils.auto(rb[i]);
-//        }
-//        rb3.setChecked(true);
     }
 
     @OnClick({R.id.rb3, R.id.rb4, R.id.rb5})
@@ -150,7 +120,10 @@ public class MainActivity extends BaseActivity {
     private void switchFragment(Fragment targetFragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         if (!targetFragment.isAdded()) {
-            transaction.hide(currentFragment).add(R.id.activity_main_ll, targetFragment).commit();
+            if(currentFragment==null)
+                transaction.add(R.id.activity_main_ll, targetFragment).commit();
+            else
+                transaction.hide(currentFragment).add(R.id.activity_main_ll, targetFragment).commit();
         } else {
             transaction.hide(currentFragment).show(targetFragment).commit();
         }
@@ -214,7 +187,6 @@ public class MainActivity extends BaseActivity {
         tv_title_center.setVisibility(View.VISIBLE);
         img_title_center.setVisibility(View.GONE);
         if(tab1==null)tab1=new TransactionFragment();
-        if(currentFragment==null)currentFragment=tab1;
         switchFragment(tab1);
     }
 
